45a4c98a08fe50cd08d90875f0d083a926a3623d
mmiesch <mark.s.miesch@gmail.com>
2022-05-10 14:55:09 -0600

    use submodule

--- submariner.py
+++ submariner.py
@@ -2,7 +2,12 @@
 The purpose of this code is to test the integration of another repo through git submodule
 """
 
+import sys
+sys.path.insert(0,'./CIMP')
 
+from CIMP import Event as ev
 
 print("Starting our submarine adventure")
+
+x = ev.event.testcase(1)
 
